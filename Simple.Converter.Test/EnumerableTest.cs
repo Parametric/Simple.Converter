72f595a516b136f403cce5ebe2782d334050a2a9
patricke <patricke@077270f5-3bd0-7742-9fc2-aea18276b555>
2012-06-04 22:35:13 +0000

    Adding unit tests for Enumerable converter
    git-svn-id: svn://gary/repos/Simple.Converter/trunk@50126 077270f5-3bd0-7742-9fc2-aea18276b555

---
+++ Simple.Converter.Test/EnumerableTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Simple.Converter.Test
+{
+    [TestFixture]
+    public class EnumerableTest
+    {
+
+        [Test]
+        public void TestEnumerableConversion()
+        {
+            SimpleConvert.SetProvider(new FakeConverterProvider());
+
+            var values = new[] {"1", "2", "3", "4"};
+            var results = values.Convert().To<int>();
+
+            Assert.That(results, Is.EquivalentTo(new[] {1,2,3,4}));
+        }
+        private class FakeConverterProvider : IConverterProvider
+        {
+            public IConvert<TFrom, TTo> Get<TFrom, TTo>()
+            {
+                object o = new LambdaConverter<string, int>(Int32.Parse);
+                return (IConvert<TFrom, TTo>)o;
+
+            }
+        }
+    }
+}
